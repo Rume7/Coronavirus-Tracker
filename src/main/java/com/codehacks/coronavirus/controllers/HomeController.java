3107e1b5698e4d87fdfa408c32e9885f1e550078
Rume7 <electengrhume@gmail.com>
2023-08-26 20:58:03 +0100

    Added attribute for totalNewCases.

--- src/main/java/com/codehacks/coronavirus/controllers/HomeController.java
+++ src/main/java/com/codehacks/coronavirus/controllers/HomeController.java
@@ -19,8 +19,10 @@ public class HomeController {
     public String home(Model model) {
         List<LocationStats> allStats = coronaVirusService.getAllStats();
         int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalStats()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
         model.addAttribute("locationStats", allStats);
         model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
         return "home";
     }
 }
