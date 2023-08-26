b9cdbb568b075857ba756b5af7288ce1c68114e1
Rume7 <electengrhume@gmail.com>
2023-08-26 20:01:54 +0100

    Added attributes for total reported cases.

--- src/main/java/com/codehacks/coronavirus/controllers/HomeController.java
+++ src/main/java/com/codehacks/coronavirus/controllers/HomeController.java
@@ -1,10 +1,13 @@
 package com.codehacks.coronavirus.controllers;
 
+import com.codehacks.coronavirus.models.LocationStats;
 import com.codehacks.coronavirus.service.CoronaVirusService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -14,7 +17,10 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("locationStats", coronaVirusService.getAllStats());
+        List<LocationStats> allStats = coronaVirusService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalStats()).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
         return "home";
     }
 }
