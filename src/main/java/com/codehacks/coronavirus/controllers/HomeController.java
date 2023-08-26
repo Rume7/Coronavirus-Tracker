d789ad0f5ce711f345cd1ff0179b039282a72537
Rume7 <electengrhume@gmail.com>
2023-08-26 19:13:38 +0100

    Added locationStats attribute to model.

--- src/main/java/com/codehacks/coronavirus/controllers/HomeController.java
+++ src/main/java/com/codehacks/coronavirus/controllers/HomeController.java
@@ -1,13 +1,20 @@
 package com.codehacks.coronavirus.controllers;
 
+import com.codehacks.coronavirus.service.CoronaVirusService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private CoronaVirusService coronaVirusService;
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("locationStats", coronaVirusService.getAllStats());
         return "home";
     }
 }
