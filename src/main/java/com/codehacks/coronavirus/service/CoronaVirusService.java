dccb54300c5854575bbc0cd35ce83e116452cd97
Rume7 <electengrhume@gmail.com>
2023-08-26 12:41:10 +0100

    Created the Service class  to fetch data.

---
+++ src/main/java/com/codehacks/coronavirus/service/CoronaVirusService.java
@@ -0,0 +1,51 @@
+package com.codehacks.coronavirus.service;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CoronaVirusService {
+
+    private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/UID_ISO_FIPS_LookUp_Table.csv";
+    private static String VIRUS_DATA_URL2 = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv";
+
+    @PostConstruct
+    public void fetchVirusData() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(VIRUS_DATA_URL2))
+                .build();
+
+        HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+        
+        Reader csvReader = new StringReader(httpResponse.body());
+        List<CSVRecord> records = CSVFormat.DEFAULT.parse(csvReader).getRecords();
+
+        int i = 0;
+        for (CSVRecord record : records) {
+            String state = record.get(0);
+            String country = record.get(1);
+            String latitude = record.get(2);
+            String longitude = record.get(3);
+
+            System.out.printf("%17s\t|\t%10s\t|\t%10s\n", country, latitude, longitude);
+            ++i;
+            if (i == 5) break;
+        }
+    }
+}
